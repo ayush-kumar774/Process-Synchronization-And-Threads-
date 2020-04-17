f8da834f694fe95880e9c260beea96b7da48e28e
Aayush Kumar <53551688+ayush-kumar774@users.noreply.github.com>
2020-04-17 14:34:10 +0530

    Update 3Thread3ResourceDeadlock.c

--- 3Thread3ResourceDeadlock.c
+++ 3Thread3ResourceDeadlock.c
@@ -8,26 +8,26 @@
 #include<pthread.h>
 #include<string.h>
 
-void *function1() ; // function declaration of first thread
-void *function2() ; // function declaration of second thread
-void *function3() ; // function declaration of third thread 
+void *function1() ; // 1st thread function
+void *function2() ; // 2nd thread function
+void *function3() ; // 3rd thread function
 
-pthread_mutex_t first_mutex ;  // first mutex variable declaration
-pthread_mutex_t second_mutex ; // second mutex variable declaration
-pthread_mutex_t third_mutex ;  // third mutex variable declaration
+pthread_mutex_t first_mutex ;  // 1 thread mutex variable
+pthread_mutex_t second_mutex ; // 2 thread mutex variable
+pthread_mutex_t third_mutex ;  // 3 thread mutex variable
 
 int main()
 {
 
-	pthread_mutex_init ( &first_mutex  , NULL  ) ; //initializing the variable to default attribute
-	pthread_mutex_init ( &second_mutex  ,NULL  ) ; //initializing the variable to default attribute
-	pthread_mutex_init ( &third_mutex  , NULL  ) ; //initializing the variable to default attribute 
+	pthread_mutex_init ( &first_mutex  , NULL  ) ;
+	pthread_mutex_init ( &second_mutex  ,NULL  ) ; 
+	pthread_mutex_init ( &third_mutex  , NULL  ) ; 
 
 	pthread_t one, two, three ; //thread declaration
 
-	pthread_create (&one,   NULL, function1, NULL) ; // creating thread one
-	pthread_create (&two,   NULL, function2, NULL) ; // creating thread two
-	pthread_create (&three, NULL, function3, NULL) ; // creating thread three
+	pthread_create (&one,   NULL, function1, NULL) ; 
+	pthread_create (&two,   NULL, function2, NULL) ; 
+	pthread_create (&three, NULL, function3, NULL) ; 
 
 
 	pthread_join (one ,  NULL) ;
@@ -40,23 +40,23 @@ int main()
 void *function1() 
 {
 
-	pthread_mutex_lock(&first_mutex); // to acquire the lock on first resource
+	pthread_mutex_lock(&first_mutex); // acquiring the lock on first resource
 	printf("Thread 1 acquired first_mutex\n");
 	sleep(1);
 	
-	pthread_mutex_lock(&second_mutex); // to acquire the lock on second resource
+	pthread_mutex_lock(&second_mutex); // acquiring the lock on second resource
 	printf("Thread 1 acquired second_mutex\n");
 	
-	pthread_mutex_lock(&third_mutex); // to acquire the lock on third resource
+	pthread_mutex_lock(&third_mutex); // acquiring the lock on third resource
 	printf("Thread 1 acquired second_mutex\n");
 	
-	pthread_mutex_unlock(&third_mutex); // to release the resource/mutex lock
+	pthread_mutex_unlock(&third_mutex); // release the resource/mutex lock from 3rd resource
 	printf("Thread 1 released third_mutex\n");
 	
-	pthread_mutex_unlock(&second_mutex); // to release the resource/mutex lock
+	pthread_mutex_unlock(&second_mutex); // release the resource/mutex lock from 2nd resource
 	printf("Thread 1 released second_mutex\n");
 	
-	pthread_mutex_unlock(&first_mutex); // to release the resource/mutex lock
+	pthread_mutex_unlock(&first_mutex); // release the resource/mutex lock from 1st resource
 	printf("Thread 1 released first_mutex\n");
 	
 }
@@ -64,22 +64,22 @@ void *function1()
 void *function2() {
 
 	pthread_mutex_lock(&second_mutex);
-	printf("Thread 2 acquired second_mutex\n");// to acquire the lock on second resource
+	printf("Thread 2 acquired second_mutex\n"); // acquiring the lock on second resource
 	sleep(1);
 
 	pthread_mutex_lock(&third_mutex);
-	printf("Thread 2 acquired third_mutex\n"); // to acquire the lock on third resource
+	printf("Thread 2 acquired third_mutex\n"); // acquiring the lock on third resource
 
 	pthread_mutex_lock(&first_mutex);
-	printf("Thread 2 acquired first_mutex\n"); // to acquire the lock on first resource
+	printf("Thread 2 acquired first_mutex\n"); // acquiring the lock on first resource
 
-	pthread_mutex_unlock(&first_mutex); // to release the resource/mutex lock
+	pthread_mutex_unlock(&first_mutex); // release the resource/mutex lock from 1st resource
 	printf("Thread 2 released first_mutex\n");
 
 	pthread_mutex_unlock(&third_mutex);
-	printf("Thread 2 acquired third_mutex\n"); // to release the resource/mutex lock
+	printf("Thread 2 acquired third_mutex\n"); // release the resource/mutex lock from 3rd resource
 
-	pthread_mutex_unlock(&second_mutex); // to release the resource/mutex lock
+	pthread_mutex_unlock(&second_mutex); // release the resource/mutex lock from 2nd resource
 	printf("Thread 2 released second_mutex\n");
 }
 
@@ -87,24 +87,24 @@ void *function3()
 {
 
 	pthread_mutex_lock (&third_mutex);
-	printf("Thread 3 acquired third_mutex\n");// to acquire the lock on third resource
+	printf("Thread 3 acquired third_mutex\n"); // acquiring the lock on third resource
 	sleep(1);
 
 	pthread_mutex_lock (&first_mutex);
-	printf("Thread 3 acquired first_mutex\n"); // to acquire the lock on first resource
+	printf("Thread 3 acquired first_mutex\n"); // acquiring the lock on first resource
 	
 	pthread_mutex_lock (&second_mutex);
-	printf("Thread 3 acquired second_mutex\n"); // to acquire the lock on second resource
+	printf("Thread 3 acquired second_mutex\n"); // acquiring the lock on second resource
 	
 	
-	pthread_mutex_unlock(&second_mutex); // to release the resource/mutex lock
+	pthread_mutex_unlock(&second_mutex); // release the resource/mutex lock from 2nd resource
 	printf("Thread 3 released second_mutex\n");
 
-	pthread_mutex_unlock(&first_mutex); // to release the resource/mutex lock
+	pthread_mutex_unlock(&first_mutex); // release the resource/mutex lock from 1st resource
 	printf("Thread 3 released first_mutex\n");
 	
 	
-	pthread_mutex_unlock(&third_mutex); // to release the resource/mutex lock
+	pthread_mutex_unlock(&third_mutex); // release the resource/mutex lock from 3rd resource
 	printf("Thread 3 released third_mutex\n");
 
 
